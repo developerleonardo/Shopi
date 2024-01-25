48cfc086d9978b7d15b89bc0249baa8d1e1b0e35
developerleonardo <leo.11hnz@gmail.com>
2024-01-25 16:13:44 -0500

    Request to the API was already made and the products are rendering on the App

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,46 @@
+import { useState, useEffect } from "react"
 import { Layout } from "../../Components/Layout"
 import { Card } from "../../Components/Card"
+import { apiUrl } from "../../api";
 
 function Home() {
+  const [items, setItems] = useState(null);
+
+  /* Si quiero usar el mismo código de la clase tengo que eliminar el import apiUrl así como esa carpeta y descomentar este código
+  
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, []) */
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/products`)
+        const data = await response.json()
+        setItems(data);
+      } catch (error) {
+        console.error(`Something went wrong: ${error}`);
+      }
+    }
+    fetchData()
+  }, [])
+
+
+
   return (
     <>
       <Layout>
         Home
-        <Card />
+        <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+          {
+            items?.map(item => (
+              <Card key={item.id} data={item} />
+            ))
+          }
+        </div>
       </Layout>
     </>
   )
