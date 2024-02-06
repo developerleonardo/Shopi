7a5de86341bace7c5c9bcbd672f3b66d971a1a05
developerleonardo <leo.11hnz@gmail.com>
2024-02-06 16:56:15 -0500

    The style of the Navbar changes depending on the value of isUserSignOut and I also made the styles of the sign-in page

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -3,120 +3,141 @@ import { ShoppingCarContext } from "../../Context";
 import { useContext } from "react";
 
 const NavBar = () => {
-    const { 
+    const {
         setSearchByCategory,
         cartProducts,
+        signOut,
         setSignOut } = useContext(ShoppingCarContext)
     const activeStyle = 'underline underline-offset-4';
+
+    // Sign Out
+    const signOutOfLocalStorage = localStorage.getItem('sign-out');
+    const parsedSignOut = JSON.parse(signOutOfLocalStorage);
+    const isUserSignOut = signOut || parsedSignOut;
 
     const handleSignOut = () => {
         const stringifiedSignOut = JSON.stringify(true)
         localStorage.setItem('sign-out', stringifiedSignOut)
         setSignOut(true)
-      } 
+    }
 
-    return (
-        <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light">
-            <ul className="flex items-center gap-3">
-                <li className="font-semibold text-lg">
-                    <NavLink to='/'>
-                        Shopi
-                    </NavLink>
-                </li>
+    const renderView = () => {
+        if (isUserSignOut) {
+            return (
                 <li>
                     <NavLink
-                        to='/'
-                        onClick={() => setSearchByCategory()}
-                        className={({ isActive }) =>
-                            isActive ? activeStyle : undefined
-                        }>
-                        All
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to='/men-clothes'
-                        onClick={() => setSearchByCategory("men's clothing")}
-                        className={({ isActive }) =>
-                            isActive ? activeStyle : undefined
-                        }>
-                        Men's Clothes
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to='/women-clothes'
-                        onClick={() => setSearchByCategory("women's clothing")}
-                        className={({ isActive }) =>
-                            isActive ? activeStyle : undefined
-                        }>
-                        Women's Clothes
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to='/electronics'
-                        onClick={() => setSearchByCategory('electronics')}
-                        className={({ isActive }) =>
-                            isActive ? activeStyle : undefined
-                        }>
-                        Electronics
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to='/jewelery'
-                        onClick={() => setSearchByCategory('jewelery')}
-                        className={({ isActive }) =>
-                            isActive ? activeStyle : undefined
-                        }>
-                        Jewelery
-                    </NavLink>
-                </li>
-            </ul>
-            <ul className="flex items-center gap-3">
-                <li className="text-black/60">
-                    user@example.com
-                </li>
-                <li>
-                    <NavLink
-                        to='/my-orders'
-                        className={({ isActive }) =>
-                            isActive ? activeStyle : undefined
-                        }>
-                        My Orders
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to='/my-account'
-                        className={({ isActive }) =>
-                            isActive ? activeStyle : undefined
-                        }>
-                        My Account
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to='/sign-in'
-                        className={({ isActive }) =>
-                            isActive ? activeStyle : undefined
-                        }
+                        to="/sign-in"
+                        className={({ isActive }) => isActive ? activeStyle : undefined}
                         onClick={() => handleSignOut()}>
-                        Sign In
+                        Sign out
                     </NavLink>
                 </li>
-                <li className="flex">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                    </svg>
-                    <div>
-                    {cartProducts.length}
-                    </div>
-                </li>
-            </ul>
-        </nav>
-    )
-};
+            )
+        } else {
+            return (
+                <>
+                    <li className='text-black/60'>
+                        teff@platzi.com
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/my-orders'
+                            className={({ isActive }) => isActive ? activeStyle : undefined}>
+                            My Orders
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/my-account'
+                            className={({ isActive }) => isActive ? activeStyle : undefined}>
+                            My Account
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/sign-in'
+                            className={({ isActive }) => isActive ? activeStyle : undefined}
+                            onClick={() => handleSignOut()}>
+                            Sign out
+                        </NavLink>
+                    </li>
+                </>
+            )
+        }
+    }
 
-export { NavBar };
+        return (
+            <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light">
+                <ul className="flex items-center gap-3">
+                    <li className="font-semibold text-lg">
+                        <NavLink to='/'>
+                            Shopi
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/'
+                            onClick={() => setSearchByCategory()}
+                            className={({ isActive }) =>
+                                isActive ? activeStyle : undefined
+                            }>
+                            All
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/men-clothes'
+                            onClick={() => setSearchByCategory("men's clothing")}
+                            className={({ isActive }) =>
+                                isActive ? activeStyle : undefined
+                            }>
+                            Men's Clothes
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/women-clothes'
+                            onClick={() => setSearchByCategory("women's clothing")}
+                            className={({ isActive }) =>
+                                isActive ? activeStyle : undefined
+                            }>
+                            Women's Clothes
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/electronics'
+                            onClick={() => setSearchByCategory('electronics')}
+                            className={({ isActive }) =>
+                                isActive ? activeStyle : undefined
+                            }>
+                            Electronics
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/jewelery'
+                            onClick={() => setSearchByCategory('jewelery')}
+                            className={({ isActive }) =>
+                                isActive ? activeStyle : undefined
+                            }>
+                            Jewelery
+                        </NavLink>
+                    </li>
+                </ul>
+                <ul className="flex items-center gap-3">
+                    { renderView() }
+                    <li className="flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                        </svg>
+                        <div>
+                            {cartProducts.length}
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        )
+    };
+
+    export { NavBar };
