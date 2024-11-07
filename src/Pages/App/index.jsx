1ceba2ef4d9efc7dc674dded23c6afaaeed3766f
developerleonardo <leo.11hnz@gmail.com>
2024-11-07 14:09:02 -0500

    Revert "bug of redirection to home was fixed - revert"
    This reverts commit 945ec677a99bb5f8194f1899b08ae4d2e15f7c21.

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,40 +1,36 @@
-import { useContext } from 'react';
-import { useRoutes, HashRouter, Navigate } from 'react-router-dom';
-import { ShoppingCarProvider, ShoppingCarContext, initializeLocalStorage } from '../../Context';
-import { Home } from '../Home';
-import { MyAccount } from '../MyAccount';
-import { MyOrder } from '../MyOrder';
-import { MyOrders } from '../MyOrders';
-import { NotFound } from '../NotFound';
-import { SignIn } from '../SignIn';
-import { NavBar } from '../../Components/NavBar';
-import { CheckoutSideMenu } from '../../Components/CheckoutSideMenu';
-import './App.css';
+import { useContext } from 'react'
+import { useRoutes, BrowserRouter, Navigate } from 'react-router-dom'
+import { ShoppingCarProvider, ShoppingCarContext, initializeLocalStorage } from '../../Context'
+import { Home } from '../Home'
+import { MyAccount } from '../MyAccount'
+import { MyOrder } from '../MyOrder'
+import { MyOrders } from '../MyOrders'
+import { NotFound } from '../NotFound'
+import { SignIn } from '../SignIn'
+import { NavBar } from '../../Components/NavBar'
+import { CheckoutSideMenu } from '../../Components/CheckoutSideMenu'
+import './App.css'
+
+
 
 const AppRoutes = () => {
   const { 
     account,
-    signOut
-  } = useContext(ShoppingCarContext);
-
+    signOut } = useContext(ShoppingCarContext)
   // Sign Out
   const signOutOfLocalStorage = localStorage.getItem('sign-out');
   const parsedSignOut = JSON.parse(signOutOfLocalStorage);
-  
   // Account
-  const accountInLocalStorage = localStorage.getItem('account');
-  const parsedAccount = JSON.parse(accountInLocalStorage);
-  
-  // Check if user has an account
-  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true;
-  const noAccountInLocalState = account ? Object.keys(account).length === 0 : true;
-  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
+  const accountInLocalStorage = localStorage.getItem('account')
+  const parsedAccount = JSON.parse(accountInLocalStorage)
+  // Has an account
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
+  const noAccountInLocalState = account ? Object.keys(account).length === 0 : true
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
   const isUserSignOut = signOut || parsedSignOut;
 
-  // Define routes with default redirect from '/' to either home or sign-in
   let routes = useRoutes([
-    { path: '/', element: <Navigate replace to={hasUserAnAccount && !isUserSignOut ? '/home' : '/sign-in'} /> },
-    { path: '/home', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
+    { path: '/', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
     { path: '/men-clothes', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
     { path: '/women-clothes', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
     { path: '/electronics', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
@@ -55,14 +51,16 @@ const App = () => {
   initializeLocalStorage();
   
   return (
-    <ShoppingCarProvider>
-      <HashRouter>
-        <AppRoutes />
-        <NavBar />
-        <CheckoutSideMenu />
-      </HashRouter>
-    </ShoppingCarProvider>
-  );
+    <>
+      <ShoppingCarProvider>
+        <BrowserRouter>
+          <AppRoutes />
+          <NavBar />
+          <CheckoutSideMenu />
+        </BrowserRouter>
+      </ShoppingCarProvider>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,59 +1,42 @@
-import { useContext } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useContext } from "react"
+import { Link } from "react-router-dom"
 import { ShoppingCarContext } from "../../Context";
-import { Layout } from "../../Components/Layout";
+import { Layout } from "../../Components/Layout"
 import { OrderCard } from "../../Components/OrderCard";
 
 function MyOrder() {
   const { order } = useContext(ShoppingCarContext);
-  const { id } = useParams(); // Use useParams to get the dynamic route parameter
-  let index = id === "last" ? order?.length - 1 : Number(id);
-
-  // Check if `order` is available and the `index` is valid
-  if (!order || isNaN(index) || index < 0 || index >= order.length) {
-    return (
-      <Layout>
-        <div className="text-center text-red-500">
-          Order not found or invalid order index.
-        </div>
-      </Layout>
-    );
-  }
+  const currentPath = window.location.pathname;
+  let index = currentPath.substring(currentPath.lastIndexOf("/") + 1);
+  if(index === "last") index = order?.length - 1
 
   return (
-    <Layout>
-      <div className="flex items-center justify-center relative w-80 mb-6">
-        <Link to="/my-orders" className="absolute left-0">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 text-black cursor-pointer"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 19.5 8.25 12l7.5-7.5"
-            />
-          </svg>
-        </Link>
-        <h1 className="font-medium text-xl">My Order</h1>
-      </div>
-      <div className="flex flex-col w-80">
-        {order[index].products.map((product) => (
-          <OrderCard
-            key={product.id}
-            id={product.id}
-            title={product.title}
-            imageUrl={product.image}
-            price={product.price}
-          />
-        ))}
-      </div>
-    </Layout>
-  );
+    <>
+      <Layout>
+        <div className="flex items-center justify-center relative w-80 mb-6">
+          <Link to='/my-orders' className="absolute left-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-black cursor-pointer">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+            </svg>
+          </Link>
+          <h1 className="font-medium text-xl">My Order</h1>
+        </div>
+        <div className='flex flex-col w-80'>
+          {
+            order?.[index]?.products.map(product => (
+              <OrderCard
+                key={product.id}
+                id={product.id}
+                title={product.title}
+                imageUrl={product.image}
+                price={product.price}
+              />
+            ))
+          }
+        </div>
+      </Layout>
+    </>
+  )
 }
 
-export { MyOrder };
+export { MyOrder }
